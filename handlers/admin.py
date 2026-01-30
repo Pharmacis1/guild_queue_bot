e06431de59958c70bab3e9b3a43b164beb1e01c2
Pharmacis1 <140200170+Pharmacis1@users.noreply.github.com>
2026-01-30 14:54:43 +0300

    Fix bug: Forced add by master no longer links unknown chars to master

--- handlers/admin.py
+++ handlers/admin.py
@@ -710,11 +710,13 @@ async def m_force_add_final(callback: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
     nick = data['nick']
     
-    char = session.query(Character).filter_by(nickname=nick).first()
-    if char: uid, main_nick = char.user_id, session.query(Character).filter_by(user_id=char.user_id, is_main=True).first().nickname
+    if char: 
+        uid = char.user_id
+        main_char = session.query(Character).filter_by(user_id=char.user_id, is_main=True).first()
+        main_nick = main_char.nickname if main_char else nick
     else: 
-        master = session.query(User).filter_by(telegram_id=callback.from_user.id).first()
-        uid, main_nick = master.id, nick
+        uid = None
+        main_nick = nick
 
     session.add(QueueEntry(user_id=uid, queue_type_id=qid, character_name=nick))
     session.commit()
